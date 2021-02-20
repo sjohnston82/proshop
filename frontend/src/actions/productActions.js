6382840d5a8f54ea5cecfd43778b05b9a17372fa
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-02-19 20:17:49 -0800

    added loader and message components to product details page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -30,23 +30,23 @@ export const listProducts = () => async (dispatch) => {
   }
 }
 
-// export const listProductDetails = (id) => async (dispatch) => { 
-//   try{
-//     dispatch({type: PRODUCT_LIST_REQUEST})
+export const listProductDetails = (id) => async (dispatch) => { 
+  try{
+    dispatch({type: PRODUCT_DETAILS_REQUEST})
 
-//     const { data } = await axios.get('/api/products/')
+    const { data } = await axios.get(`/api/products/${id}`)
 
-//     dispatch({
-//       type: PRODUCT_LIST_SUCCESS,
-//       payload: data
-//     })
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    })
 
-//   }catch(error){
-//     dispatch({
-//       type: PRODUCT_LIST_FAIL,
-//       payload: error.response && error.response.data.message 
-//       ? error.response.data.message
-//       : error.message,
-//     })
-//   }
-// }
+  }catch(error){
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response && error.response.data.message 
+      ? error.response.data.message
+      : error.message,
+    })
+  }
+}
